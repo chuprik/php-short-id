662bf94d9e2baf07cd04a11c06ece599ab853425
Constantin Chuprik <constantinchuprik@gmail.com>
2015-05-08 21:05:52 +0600

    initial

---
+++ README.md
@@ -0,0 +1,37 @@
+# PHP Short ID creator
+
+The library help you generate short id like youtube, vimeo, bit.ly, etc. Short generation (creation) based on numerical ID. 
+
+## Simple scenarios of using
+
+```php
+require('vendor/autoload.php');
+
+$shortId = new \kotchuprik\short_id\ShortId();
+```
+ 
+### Creating short ID for a record from in a database
+
+1) when an app created a record in an your database with ID 424242 
+2) $shortId->encode(424242) encodes it to 'bLTs'
+3) you updated the record for ID 424242 and set short_id of the record to 'bLTs'
+
+```php
+$id = $shortId->encode(422424);     // $id will be 'bLTs'
+
+// or with $neededLength = 6
+$id = $shortId->encode(422424, 6);  // $id will be 'babMwC'
+```
+
+### Searching record in a database 
+
+1) when someone requests rLHWfKd
+2) $shortId->decode('rLHWfKd') decodes it to 424242
+3) you found the record for ID 424242 in an your database
+
+```php
+$id = $shortId->encode('bLTs');      // $id will be 'bLTs'
+
+// or with $neededLength = 6
+$id = $shortId->encode('babMwC', 6); // $id will be 'babMwC'
+```
---
+++ ShortId.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace kotchuprik\short_id;
+
+class ShortId
+{
+    protected $alphabet;
+
+    public function __construct($alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $this->alphabet = $alphabet;
+    }
+
+    public function encode($input, $neededLength = 0)
+    {
+        $output = '';
+        $base = strlen($this->alphabet);
+        if (is_numeric($neededLength)) {
+            $neededLength--;
+            if ($neededLength > 0) {
+                $input += pow($base, $neededLength);
+            }
+        }
+        for ($current = ($input != 0 ? floor(log($input, $base)) : 0); $current >= 0; $current--) {
+            $bcpowed = bcpow($base, $current);
+            $floored = floor($input / $bcpowed) % $base;
+            $output = $output . substr($this->alphabet, $floored, 1);
+            $input = $input - ($floored * $bcpowed);
+        }
+
+        return $output;
+    }
+
+    public function decode($input, $neededLength = 0)
+    {
+        $output = 0;
+        $base = strlen($this->alphabet);
+        $length = strlen($input) - 1;
+        for ($current = $length; $current >= 0; $current--) {
+            $bcpowed = bcpow($base, $length - $current);
+            $output = ($output + strpos($this->alphabet, substr($input, $current, 1)) * $bcpowed);
+        }
+        if (is_numeric($neededLength)) {
+            $neededLength--;
+            if ($neededLength > 0) {
+                $output -= pow($base, $neededLength);
+            }
+        }
+
+        return $output;
+    }
+}

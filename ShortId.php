7d730f026b9a6a6fcff5c766e2c193752e6e7f51
Constantine Chuprik <constantinchuprik@gmail.com>
2016-03-25 11:15:12 +0300

    bcpow() replaced to pow()

--- ShortId.php
+++ ShortId.php
@@ -22,10 +22,10 @@ class ShortId
             }
         }
         for ($current = ($input != 0 ? floor(log($input, $base)) : 0); $current >= 0; $current--) {
-            $bcpowed = bcpow($base, $current);
-            $floored = floor($input / $bcpowed) % $base;
+            $powed = pow($base, $current);
+            $floored = floor($input / $powed) % $base;
             $output = $output . substr($this->alphabet, $floored, 1);
-            $input = $input - ($floored * $bcpowed);
+            $input = $input - ($floored * $powed);
         }
 
         return $output;
@@ -37,8 +37,8 @@ class ShortId
         $base = strlen($this->alphabet);
         $length = strlen($input) - 1;
         for ($current = $length; $current >= 0; $current--) {
-            $bcpowed = bcpow($base, $length - $current);
-            $output = ($output + strpos($this->alphabet, substr($input, $current, 1)) * $bcpowed);
+            $powed = pow($base, $length - $current);
+            $output = ($output + strpos($this->alphabet, substr($input, $current, 1)) * $powed);
         }
         if (is_numeric($neededLength)) {
             $neededLength--;
